a51d2f7c1a0f91dbc17c69bbd399133e2e5a7a5a
Jacob Cavazos <jjcav84@gmail.com>
2019-05-21 21:04:56 -0500

    added query to pull down all characters from API in index.js tweaked size of logo, and edited site metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,36 +5,49 @@ import Layout from '../components/layout'
 import EmployeeCard from '../components/EmployeeCard'
 
 const h1Style = {
-  marginTop: '135px',
+  marginTop: '115px',
   marginBottom: '-15px'
 }
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <Helmet>
-      <title>{data.site.siteMetadata.title}</title>
-      <meta
-        name="description"
-        content="Static single page web app directory site built with React, MDBootstrap, and Gatsby"
-      />
-      <meta name="keywords" content="React, Bootstrap, Gatsby, Material Design, directory" />
-      <meta name="author" content="Jacob Cavazos" />
-      <link rel="canonical" href="" />
-    </Helmet>
-    <div style={h1Style} className="text-center pb-4">
-      <h1>{data.site.siteMetadata.title}</h1>
-    </div>
-    <div className="pb-4">
-      <EmployeeCard />
-    </div>
-  </Layout>
-)
-
-export const query = graphql`
-  query {
+const IndexPage = props => {
+  return (
+    <Layout>
+      <Helmet>
+        <title>{props.data.site.siteMetadata.title}</title>
+        <meta name="description" content={props.data.site.siteMetadata.description} />
+        <meta name="keywords" content={props.data.site.siteMetadata.keywords} />
+        <meta name="author" content={props.data.site.siteMetadata.author} />
+        <link rel="canonical" href={props.data.site.siteMetadata.siteUrl} />
+      </Helmet>
+      <div style={h1Style} className="text-center pb-4">
+        <h1>{props.data.site.siteMetadata.title}</h1>
+      </div>
+      <div className="pb-4">
+        <EmployeeCard />
+      </div>
+    </Layout>
+  )
+}
+export const pageQuery = graphql`
+  query IndexQuery {
     site {
       siteMetadata {
         title
+        author
+        description
+        siteUrl
+        keywords
+      }
+    }
+    rickAndMortyAPI {
+      characters {
+        results {
+          name
+          image
+          status
+          species
+          gender
+        }
       }
     }
   }
