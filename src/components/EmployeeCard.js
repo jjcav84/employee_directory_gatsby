30f34e093fad931f8df218cd18da5489b39912a9
Jacob Cavazos <jjcav84@gmail.com>
2019-05-23 03:46:16 -0500

    edited employeecard component to use props that I will pass from the index graphql data

--- src/components/EmployeeCard.js
+++ src/components/EmployeeCard.js
@@ -1,41 +1,26 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
 import { MDBCard, MDBCardBody, MDBCardImage, MDBCardTitle, MDBCardText, MDBCol, MDBRow } from 'mdbreact'
 
-export default () => (
-  <StaticQuery
-    query={graphql`
-      {
-        rickAndMortyAPI {
-          character(id: 1) {
-            id
-            name
-            image
-            status
-            species
-            gender
-          }
-        }
-      }
-    `}
-    render={data => (
-      <MDBRow className="mx-auto">
-        <MDBCol>
-          <MDBCard className="mx-auto" style={{ width: '19rem' }}>
-            <div>
-              <MDBCardImage src={data.rickAndMortyAPI.character.image} waves hover top />
-            </div>
-            <MDBCardBody>
-              <MDBCardTitle>{data.rickAndMortyAPI.character.name}</MDBCardTitle>
-              <MDBCardText>
-                <p className="border-bottom border-dark">{data.rickAndMortyAPI.character.status}</p>
-                <p className="border-bottom border-dark">{data.rickAndMortyAPI.character.species}</p>
-                <p className="border-bottom border-dark">{data.rickAndMortyAPI.character.gender}</p>
-              </MDBCardText>
-            </MDBCardBody>
-          </MDBCard>
-        </MDBCol>
-      </MDBRow>
-    )}
-  />
-)
+const EmployeeCard = props => {
+  return (
+    <MDBRow className="mx-auto">
+      <MDBCol>
+        <MDBCard className="mx-auto" style={{ width: '19rem' }}>
+          <div>
+            <MDBCardImage src={props.image} waves hover top />
+          </div>
+          <MDBCardBody>
+            <MDBCardTitle>{props.name}</MDBCardTitle>
+            <MDBCardText>
+              <p className="border-bottom border-dark">{props.status}</p>
+              <p className="border-bottom border-dark">{props.species}</p>
+              <p>{props.gender}</p>
+            </MDBCardText>
+          </MDBCardBody>
+        </MDBCard>
+      </MDBCol>
+    </MDBRow>
+  )
+}
+
+export default EmployeeCard
