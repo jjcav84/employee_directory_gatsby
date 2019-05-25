1045a36b783113a09ef8019d8e9ffc2af152b269
Jacob Cavazos <jjcav84@gmail.com>
2019-05-25 14:42:41 -0500

    commented out template and gatsby-node.js will return to schema and pagination later

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,1 +1,22 @@
-//
+/* exports.createPages = async function({ actions, graphql }) {
+  const { data } = await graphql(`
+    query {
+      rickAndMortyAPI {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+  data.rickAndMortyAPI.edges.forEach(edge => {
+    const node = edge.node
+    actions.createPage({
+      path: node,
+      component: require.resolve(`./src/templates/page.js`),
+      context: { node: node },
+    })
+  })
+}
+ */
