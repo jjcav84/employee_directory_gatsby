6bdcbe18f6a0777bdcecfdb77391d0028d452046
Jacob Cavazos <jjcav84@gmail.com>
2019-05-27 23:27:07 -0500

    increased padding-right for the card on the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -54,7 +54,7 @@ const DirectoryPage = props => (
         <div className="float-right pt-2">
           <MDBRow>
             <a href="https://algolia.com">
-              <img src={algolia} className="pr-1" />
+              <img src={algolia} className="pr-3" />
             </a>
             <SearchBox />
           </MDBRow>
